7bbb35be2194c28fdf2917f37d7ca0456e092e68
TijmenSchouten <5952071-TijmenSchouten@users.noreply.replit.com>
2023-04-14 15:31:36 +0000

    js code op orde maken

---
+++ index.js
@@ -0,0 +1,13 @@
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 5;
+let velocityX = 0, velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
