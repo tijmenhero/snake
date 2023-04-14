964a76d6fa9c4050e4601aa7ede766df410d9c4e
TijmenSchouten <5952071-TijmenSchouten@users.noreply.replit.com>
2023-04-14 15:55:52 +0000

    snelheid verandering bij input Arrow keys

--- index.js
+++ index.js
@@ -27,3 +27,21 @@ const handleGameOver = () =>{
   alert("Game Over! Press OK to replay...");
   location.reload();
 }
+
+const changeDirection = e =>{
+  if(e.key === "ArrowUp" && velocityY !=1){
+    velocityX = 0;
+    velocityY = -1:
+  }else if(e.key === "ArrowDown" && volocityY != -1){
+    velocityX = 0;
+    velocityY = 1;
+  }else if(e.key === "ArrowLeft" && velocityX != 1){
+    velocityX = -1;
+    velocityY = 0;
+  }else if(e.key === "ArrowRight" && velocityX != -1){
+    velocityX = 1;
+    velocityY = 0;
+  }
+}
+
+
