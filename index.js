23bdb6636927f297f48c77e9b8ea7b28d3b6a681
TijmenSchouten <5952071-TijmenSchouten@users.noreply.replit.com>
2023-04-14 17:28:46 +0000

    sound.js toegevoegd zodat we geluiden kunnen gebruiken

--- index.js
+++ index.js
@@ -12,82 +12,87 @@ let setIntervalId;
 let score = 0;
 
 
-
 let highScore = localStorage.getItem("high-score") || 0;
-highScoreElement.innerText = 'High Score: ${highScore}';
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 
-const updatFoodPosition = () =>{
-  foodX = Math.floor(Math.random() * 30) + 1;
-  foodY = Math.floor(Math.random() * 30) + 1;
+const updateFoodPosition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
 }
 
-const handleGameOver = () =>{
-  clearInterval(setIntervalId);
-  alert("Game Over! Press OK to replay...");
-  location.reload();
+const handleGameOver = () => {
+    clearInterval(setIntervalId);
+    alert("Game Over! Press OK to replay...");
+    location.reload();
 }
 
-const changeDirection = e =>{
-  if(e.key === "ArrowUp" && velocityY !=1){
-    velocityX = 0;
-    velocityY = -1:
-  }else if(e.key === "ArrowDown" && volocityY != -1){
-    velocityX = 0;
-    velocityY = 1;
-  }else if(e.key === "ArrowLeft" && velocityX != 1){
-    velocityX = -1;
-    velocityY = 0;
-  }else if(e.key === "ArrowRight" && velocityX != -1){
-    velocityX = 1;
-    velocityY = 0;
-  }
-}
 
-controls.forEach(button => button.addEventListener("click", () => changeDirection({key: button.dataset.key})));
-
-const initGame = () =>{
-  if(gameOver) return handleGameOver();
-  let html = '<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>';
-
-  
-  if(snakeX === foodX && snakeY === foodY){
-    updateFoodPosition();
-    snakeBody.push([foodY, foodX]); //food bij snakebody optellen
-    score++;
-    highScore = score >= highScore ? score : highScore // als score groter is dan high score dan wordt de high score die score
-
-    localStorage.setItem("high-score", highScore);
-    scoreElement.innerText = 'Score: ${score}';
-    highScoreElement.innerText = 'High Score: ${highScore}';
-  }
-
-  // update snake head 
-  snakeX += velocityX;
-  snakeY += velocityY;
-
-  for(let i = snakeBody.length - 1; i > 0; i--){
-    snakeBody[i] = snakeBody[i - 1];  
-  }
-
-  snakeBody[0] = [snakeX, snakeY];
-
-  // checken of snake body buiten de muren is of niet 
-
-  if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
-    return gameOver = true;
-  }
-
-  // div toevoegen voor elk deel van de snake body
-  
-  for(let i = 0; i < snakeBody.length; i++){
-    html += '<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>';
-    // checken of snake head snake body raakt  of niet
-    if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
-      gameOver = true;
+const changeDirection = e => {
+    if (e.key === "ArrowUp" && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
     }
-  }
-
-  
 }
 
+
+controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
+
+const initGame = () => {
+    if (gameOver) return handleGameOver();
+    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    // als snake food eet 
+    if (snakeX === foodX && snakeY === foodY) {
+        updateFoodPosition();
+        snakeBody.push([foodY, foodX]); 
+        score++;
+        highScore = score >= highScore ? score : highScore; // als score > highscore dan wordt die score de highscore 
+
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
+    }
+
+   // update snake head 
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+  
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    // Check snake body binnen de muren of niet 
+
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+        return gameOver = true;
+    }
+
+    // div toevoegen voor elk stukje snake body
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+        // Check snake head raakt body of niet
+        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
+            gameOver = true;
+        }
+    }
+    playBoard.innerHTML = html;
+}
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
